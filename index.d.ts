20df96a1d1941baa8298d0847100999539addad0
ravi18 <ravi@arrowai.com>
2024-05-16 10:13:50 +0530

    1.0.5 added

--- index.d.ts
+++ index.d.ts
@@ -1,4 +1,9 @@
 import { ChatMessage } from './types/chatMessage';
 
 export { ChatMessage };
-export * from './types/browser'
+export * from './types/browser';
+export * from './types/fs';
+export * from './types/codeutils';
+export * from './types/dbmemorty';
+export * from './types/state';
+export * from './types/vectordb'
